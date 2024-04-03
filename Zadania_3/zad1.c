379f08cd8b5ecb6ed933942800e3fc2527976994
Maciej Kasperczyk <makasperczyk1@gmail.com>
2024-04-03 19:21:48 +0200

    Update zad1.c

--- Zadania_3/zad1.c
+++ Zadania_3/zad1.c
@@ -1,64 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-int** alokujMacierz(int wiersze, int kolumny) {
-    int** macierz = malloc(wiersze * sizeof(int*));
-    if (macierz == NULL) {
-        printf("Błąd alokacji pamięci dla wierszy.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    for (int i = 0; i < wiersze; i++) {
-        macierz[i] = malloc(kolumny * sizeof(int));
-        if (macierz[i] == NULL) {
-            printf("Błąd alokacji pamięci dla kolumn.\n");
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    return macierz;
+int wspolczynnikDwumianowy(int n, int k) {
+    if (k == 0 || k == n)
+        return 1;
+    return wspolczynnikDwumianowy(n - 1, k - 1) + wspolczynnikDwumianowy(n - 1, k);
 }
 
-void wypelnijMacierz(int** macierz, int wiersze, int kolumny) {
-    srand(time(NULL));
-
-    for (int i = 0; i < wiersze; i++) {
-        for (int j = 0; j < kolumny; j++) {
-            macierz[i][j] = rand() % 100; 
-        }
-    }
-}
-
-void wyswietlMacierz(int** macierz, int wiersze, int kolumny) {
-    printf("Zawartość macierzy:\n");
-    for (int i = 0; i < wiersze; i++) {
-        for (int j = 0; j < kolumny; j++) {
-            printf("%d ", macierz[i][j]);
+void wyswietlTrojkatPascala(int N) {
+    int i, j;
+    for (i = 0; i < N; i++) {
+        for (j = 0; j <= i; j++) {
+            printf("%d ", wspolczynnikDwumianowy(i, j));
         }
         printf("\n");
     }
 }
 
-void zwolnijMacierz(int** macierz, int wiersze) {
-    for (int i = 0; i < wiersze; i++) {
-        free(macierz[i]);
-    }
-    free(macierz);
-}
-
 int main() {
-    int wiersze, kolumny;
-
-    printf("Podaj liczbę wierszy macierzy: ");
-    scanf("%d", &wiersze);
-    printf("Podaj liczbę kolumn macierzy: ");
-    scanf("%d", &kolumny);
-
-    int** macierz = alokujMacierz(wiersze, kolumny);
-    wypelnijMacierz(macierz, wiersze, kolumny);
-    wyswietlMacierz(macierz, wiersze, kolumny);
-    zwolnijMacierz(macierz, wiersze);
-
+    int N;
+    printf("Podaj wysokość trójkąta Pascala: ");
+    scanf("%d", &N);
+    wyswietlTrojkatPascala(N);
     return 0;
 }
